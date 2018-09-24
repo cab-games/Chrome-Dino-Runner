005bdb1a1d8eab47f1844db4918788799e929610
cab-games <98125243+cab-games@users.noreply.github.com>
2018-09-24 12:38:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// extract from chromium source code by @liuwayong
+// Extracted from the Chromium source code and edited by @CCreativeCND.
+
 (function () {
     'use strict';
     /**
